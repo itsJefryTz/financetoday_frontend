ae70df391d2443528923856960d666d2639b7784
Jefry Urdaneta <167945772+itsJefryTz@users.noreply.github.com>
2025-03-19 04:23:30 -0400

    Update ProtectedRoute.jsx

--- src/components/session/ProtectedRoute.jsx
+++ src/components/session/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 const ProtectedRoute = ({ children }) => {
     const isAuthenticated = !!localStorage.getItem('user_token_access') && !!localStorage.getItem('user_token_refresh');
 
-    return isAuthenticated ? children : <Navigate to="/#/login" />;
+    return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
